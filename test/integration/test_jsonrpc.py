874efdb257aa7f694e82ec984db1cfc53d179fe8
TaylorFrazier <41766869+TaylorFrazier@users.noreply.github.com>
2018-09-07 18:12:23 -0400

    Update test_jsonrpc.py

--- test/integration/test_jsonrpc.py
+++ test/integration/test_jsonrpc.py
@@ -16,7 +16,7 @@ def test_emralsd():
     config_text = EmralsConfig.slurp_config_file(config.emrals_conf)
     network = 'mainnet'
     is_testnet = False
-    genesis_hash = u'00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6'
+    genesis_hash = u'00000fbe71ccb50a95cc784f5dc8be27877ebec894b061c202f4bb74c84506bd'
     for line in config_text.split("\n"):
         if line.startswith('testnet=1'):
             network = 'testnet'
